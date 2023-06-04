fb229491ae5b8e779c6bb0bf7785b95971f66f90
Samuel Uyigue <Samuel.uyigue@gmail.com>
2023-06-04 09:21:44 -0500

    MatchedGeo effect from collapsed to expand mode animation
    - refactored musicinfoView
    - imported assets

--- AppleMusic/ExpandedBottomSheet.swift
+++ AppleMusic/ExpandedBottomSheet.swift
@@ -11,31 +11,53 @@ struct ExpandedBottomSheet: View {
     @Binding var expandSheet: Bool
     var animation: Namespace.ID
     @State private var animateContent = false
-
+    
     var body: some View {
         GeometryReader {
             let size = $0.size
             let safeArea = $0.safeAreaInsets
             
             ZStack {
-                dragIndicator()
-                    .opacity(animateContent ? 1 : 0)
                 
-                /// Artwork with Hero Animation
-                GeometryReader {
-                    let size = $0.size
+                Rectangle()
+                    .fill(.ultraThickMaterial)
+                    .overlay {
+                        Rectangle()
+                            .fill(Color("BG"))
+                            .opacity(animateContent ? 1 : 0)
+                    }
+                    .overlay(alignment: .top) {
+                        MusicInfoView(expandSheet: $expandSheet, animation: animation)
+                            .allowsHitTesting(false) /// Disabling interaction
+                            .opacity(animateContent ? 0 : 1)
+                    }
+                    .matchedGeometryEffect(id: "BGVIEW", in: animation)
+                
+                VStack(spacing: 15) {
+                    dragIndicator()
+                        .opacity(animateContent ? 1 : 0)
                     
-                    MusicArtworkImageView(image: "p2", size: size, cornerRadius: 15)
+                    /// Artwork with Hero Animation
+                    GeometryReader {
+                        let size = $0.size
+                        MusicArtworkImageView(image: "p2", size: size, cornerRadius: 15)
+                    }
+                    .matchedGeometryEffect(id: "ARTWORK", in: animation)
+                    .frame(height: size.width - 50)
                 }
-                .matchedGeometryEffect(id: "ARTWORK", in: animation)
-                .frame(height: size.width - 50)
-                
+                .padding(.top, safeArea.top + (safeArea.bottom == 0 ? 10 : 0))
+                .padding(.bottom, safeArea.bottom == 0 ? 10 : safeArea.bottom)
+                .padding(.horizontal, 25)
+                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
+                .onTapGesture {
+                    withAnimation(.easeInOut(duration: 0.3)) {
+                        expandSheet = false
+                        animateContent = false
+                    }
+                }
             }
-            .padding(.top, safeArea.top + (safeArea.bottom == 0 ? 10 : 0))
-            .padding(.bottom, safeArea.bottom == 0 ? 10 : safeArea.bottom)
-            .padding(.horizontal, 25)
-            .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
             .ignoresSafeArea(.container, edges: .all)
+
         }
         .onAppear {
             withAnimation(.easeInOut(duration: 0.35)) {
---
+++ AppleMusic/MusicInfoView.swift
@@ -0,0 +1,69 @@
+//
+//  MusicInfoView.swift
+//  AppleMusic
+//
+//  Created by Osaretin Uyigue on 6/4/23.
+//
+
+import SwiftUI
+
+struct MusicInfoView: View {
+    @Binding var expandSheet: Bool
+    var animation: Namespace.ID
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            /// Matched Geo Hero Animation
+            ZStack {
+                if !expandSheet {
+                    GeometryReader {
+                        let size = $0.size
+                        MusicArtworkImageView(image: "p2", size: size)
+                    }
+                    .matchedGeometryEffect(id: "ARTWORK", in: animation)
+                }
+            }
+            .frame(width: 45, height: 45)
+            
+            Text("Look What You Made Me do")
+                .fontWeight(.semibold)
+                .lineLimit(1)
+                .padding(.horizontal, 15)
+            
+            Spacer()
+            
+            generateButton(for: "pause.fill")
+            
+            generateButton(for: "forward.fill")
+                .padding(.leading, 25)
+            
+        }
+        .foregroundColor(.primary)
+        .padding(.horizontal)
+        .padding(.bottom, 5)
+        .frame(height: 70)
+        .contentShape(Rectangle())
+        .onTapGesture {
+            withAnimation(.easeInOut(duration: 0.3)) {
+                expandSheet = true
+            }
+        }
+    }
+    
+    @ViewBuilder
+    private func generateButton(for systemImage: String) -> some View {
+        Button {
+            //
+        } label: {
+            Image(systemName: systemImage)
+                .font(.title2)
+        }
+    }
+}
+
+
+struct MusicInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+       HomeView()
+    }
+}
