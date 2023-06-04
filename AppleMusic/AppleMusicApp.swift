d19d32d5b524ef1685f4f43b23fd4c832471c9c6
Samuel Uyigue <Samuel.uyigue@gmail.com>
2023-06-04 07:32:06 -0500

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Osaretin Uyigue on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
